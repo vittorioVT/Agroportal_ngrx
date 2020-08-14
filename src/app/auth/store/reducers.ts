484da3c3086cd80a2364195bad43f3237f27613c
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-08-14 10:52:20 +0300

    Added reducers for register

---
+++ src/app/auth/store/reducers.ts
@@ -0,0 +1,19 @@
+import { AuthStateInterface } from "src/app/auth/types/authState.interface";
+import { createReducer, on, Action } from "@ngrx/store";
+import { registerAction } from "./actions/register.action";
+
+const initialState: AuthStateInterface = {
+  isSubmitting: false
+};
+
+const authReducer = createReducer(
+  initialState,
+  on(registerAction, (state): AuthStateInterface => ({
+    ...state,
+    isSubmitting: true
+  }))
+);
+
+export function reducers(state: AuthStateInterface, action: Action) {
+  return authReducer(state, action);
+}
